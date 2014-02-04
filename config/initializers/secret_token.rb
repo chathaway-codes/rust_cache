4622942ef66710399cf27addd7e9463894b5c9bf
Charles Hathaway <chathaway@logrit.com>
2014-02-04 14:46:44 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RustCache::Application.config.secret_key_base = '7cd9b6b1d8d92b386be88e1fbf3265df0f52b3ffeb1bc2ecd407586abc58048da2491ffed983bc4ecde340f9956c42452b3e20c114fc641165e4fcaf376e8aa0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RustCache::Application.config.session_store :cookie_store, key: '_rust_cache_session'
