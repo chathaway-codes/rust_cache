7718e7d0ade15a853b80f2e6463b0d1ed357b9e6
Charles Hathaway <chathaway@logrit.com>
2014-02-04 19:43:35 -0500

    Should require authentication for bounties

--- app/controllers/bounties_controller.rb
+++ app/controllers/bounties_controller.rb
@@ -1,5 +1,6 @@
 class BountiesController < ApplicationController
   before_action :set_bounty, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!
 
   # GET /bounties
   # GET /bounties.json
